8a4d431bd3e2f360880d2e265b41fe8c4887bb2d
Rani Abu Sultan <raniabusultan@gmail.com>
2018-12-02 13:05:36 +0300

    Angular Built-In Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,16 @@ export class AppComponent {
 
   courses = COURSES;
 
+  title = COURSES[1].description;
+
+  price = 9.993513462;
+
+  rate = 0.67;
+
+  course2 = COURSES[1];
+
+  startDate = new Date(2000, 0, 1);
+
   onCourseSelected(course: Course) {
     console.log('App component - click event bubbled...', course);
   }
