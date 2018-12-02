72127d46cca15b3c98b96a1e32b65e9ce0cbc6cc
Rani Abu Sultan <raniabusultan@gmail.com>
2018-12-03 02:49:30 +0300

    View Child - Learn the multiple View Query Configuration Options

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import {COURSES} from '../db-data';
 import { Course } from './model/course';
 import { CourseCardComponent } from './course-card/course-card.component';
@@ -12,13 +12,21 @@ export class AppComponent {
 
   courses = COURSES;
 
-  @ViewChild(CourseCardComponent)
-  card: CourseCardComponent;
+  @ViewChild('cardRef')
+  card1: CourseCardComponent;
+
+  @ViewChild('cardRef', {read: ElementRef})
+  card2: CourseCardComponent;
+
+  @ViewChild('container')
+  containerDiv: ElementRef;
 
   onCourseSelected(course: Course) {
     // console.log('App component - click event bubbled...', course);
 
-    console.log(this.card);
+    console.log(this.card1);
+    console.log(this.card2);
+    console.log(this.containerDiv);
   }
 
 }
