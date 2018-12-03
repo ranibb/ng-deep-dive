153844594be56fa2f5075f71e277d377877af06c
Rani Abu Sultan <raniabusultan@gmail.com>
2018-12-03 19:42:50 +0300

    The View Children Decorator and Query List In Detail

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit, ViewChildren, QueryList } from '@angular/core';
 import {COURSES} from '../db-data';
 import { Course } from './model/course';
+import { CourseCardComponent } from './course-card/course-card.component';
+
 
 @Component({
   selector: 'app-root',
@@ -11,18 +13,45 @@ export class AppComponent implements AfterViewInit {
 
   courses = COURSES;
 
-  @ViewChild('cardRef', {read: ElementRef})
-  card: ElementRef;
+  @ViewChildren(CourseCardComponent)
+  cardsCardComponent: QueryList<CourseCardComponent>;
 
-  @ViewChild('courseImage')
-  image: ElementRef;
+  @ViewChildren(CourseCardComponent, {read: ElementRef})
+  cardsElementRef: QueryList<ElementRef>;
 
   constructor() {
   }
 
 
   ngAfterViewInit() {
-    console.log(this.image);
+
+    console.log(this.cardsCardComponent);
+    console.log(this.cardsCardComponent.first);
+
+    console.log(this.cardsElementRef);
+    console.log(this.cardsElementRef.first);
+
+
+    this.cardsCardComponent.changes.subscribe(
+      cards => console.log(cards)
+    );
+
+    this.cardsElementRef.changes.subscribe(
+      cards => console.log(cards)
+    );
+  }
+
+  onCoursesEdited() {
+    this.courses.push(
+      {
+        id: 1,
+        description: 'Angular core deep dive',
+        iconUrl: 'https://s3-us-west-1.amazonaws.com/angular-university/course-images/angular-core-in-depth-small.png',
+        longDescription: 'A detailed walk-through of the most important part of Angular - the Core and Common modules',
+        category: 'INTERMEDIATE',
+        lessonsCount: 10
+      }
+    );
   }
 
   onCourseSelected(course: Course) {
