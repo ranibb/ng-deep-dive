20239625a492932d901ca223f0f538efe0106218
Rani Abu Sultan <raniabusultan@gmail.com>
2018-12-05 17:34:55 +0300

    Angular Content Children and the AfterContentInit Lyfecycle Hook

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, EventEmitter, Output, ContentChild, AfterViewInit, ElementRef } from '@angular/core';
+import { Component, Input, EventEmitter, Output, ContentChild, AfterViewInit, ElementRef, ContentChildren, QueryList, AfterContentInit } from '@angular/core';
 import { Course } from '../model/course';
 import { CourseImageComponent } from '../course-image/course-image.component';
 
@@ -7,16 +7,22 @@ import { CourseImageComponent } from '../course-image/course-image.component';
   templateUrl: './course-card.component.html',
   styleUrls: ['./course-card.component.css']
 })
-export class CourseCardComponent implements AfterViewInit {
+export class CourseCardComponent implements AfterViewInit, AfterContentInit {
 
   @Input()
   course: Course;
+
+  @Input()
+  cardIndex: number;
 
   @Output('courseSelected')
   courseSelected = new EventEmitter<Course>();
 
   @ContentChild(CourseImageComponent, {read: ElementRef})
   image: ElementRef;
+
+  @ContentChildren(CourseImageComponent, {read: ElementRef})
+  images: QueryList<ElementRef>;
 
   @ContentChild('courseDesc')
   courseDesc;
@@ -28,9 +34,25 @@ export class CourseCardComponent implements AfterViewInit {
     console.log(this.courseDesc);
   }
 
+  ngAfterContentInit() {
+    console.log(this.images);
+  }
+
   onCourseViewed() {
     console.log('card component - button clicked ....');
     this.courseSelected.emit(this.course);
+  }
+
+  cardClasses() {
+    if (this.course.category === 'BEGINNER') {
+      return ['beginner'];
+    }
+  }
+
+  cardStyles() {
+    return {
+      // 'background-image': 'url(' + this.course.iconUrl + ')'
+    };
   }
 
 }
